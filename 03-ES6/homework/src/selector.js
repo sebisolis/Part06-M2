985f6235d6ca8313e73f599f497a1f8724446a06
Sebastian Solis <sebasolis@users.noreply.github.com>
2022-04-23 18:42:04 -0300

    commit HW 02 y 03

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,15 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if(matchFunc(startEl)) resultSet.push(startEl);
+
+  for (let i = 0; i < startEl.children.length; i++) {
+    let child = startEl.children[i];
+    let found = traverseDomAndCollectElements(matchFunc, child);
+    resultSet = [...resultSet, ...found];
+  }
+
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -19,6 +27,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
   
+  if(selector[0] === "#") return "id";
+  if(selector[0] === ".") return "class";
+  if(selector.split(".").length === 2) return "tag.class";
+  return "tag";
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +42,32 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+   matchFunction = function(element) {
+     // Verificamos si [element] tiene el ID que pasé en el selector.
+     return '#' + element.id === selector;
+   }
   } else if (selectorType === "class") {
-    
+    matchFunction = function(element) {
+      // Verificamos si [element] tiene la CLASE que pasé en el selector.
+      let classes = element.classList;
+      for (let i = 0; i < classes.length; i++) {
+        if('.' + classes[i] === selector) {
+          return true;
+        };
+      }
+      return false;
+    }    
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = function(element) {
+      // Verificamos si [element] tiene el TAG y la CLASE que pasé en el selector.
+      let [t, c] = selector.split('.'); // ['tag', 'class']
+      return matchFunctionMaker(t)(element) && matchFunctionMaker('.' + c)(element);
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = function(element) {
+      // Verificamos si [element] tiene el TAG que pasé en el selector.
+      return element.tagName === selector.toUpperCase();
+    }    
   }
   return matchFunction;
 };
@@ -47,3 +78,8 @@ var $ = function(selector) {
   elements = traverseDomAndCollectElements(selectorMatchFunc);
   return elements;
 };
+
+document.querySelector('#idOne'); // id
+document.querySelector('.classOne'); // class one
+document.querySelector('div'); // tag
+document.querySelector('div.classOne'); // tag.class
